fab5c5fac4b8a0f417b0ee3e8482d76e58bc6f70
unknown <dafraouimehdi05@gmail.com>
2024-06-09 15:33:49 +0100

    commited all files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './Navigate/Navbar';
+import { Provider } from 'react-redux';
+import store from './ReduxToolkit/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>
 );
 
